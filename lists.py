5e438587002ec0d9f6c3e105b2e90c4317e36664
rokib97 <rokibulhasan.ph@gmail.com>
2025-01-05 05:46:50 +0600

    solved lists problem ;_;

--- lists.py
+++ lists.py
@@ -1,4 +1,18 @@
-n = int(input())
-integer_list = map(int, input().split())
-integer_tuple = tuple(integer_list)
-print(hash(integer_tuple))
+n = int(input(""))
+my_list = []
+for i in range(n):
+    latest_command = input().split()
+    if latest_command[0] == 'insert':
+        my_list.insert(int(latest_command[1]), int(latest_command[2]))
+    elif latest_command[0] == 'remove':
+        my_list.remove(int(latest_command[1]))
+    elif latest_command[0] == 'append':
+        my_list.append(int(latest_command[1]))
+    elif latest_command[0] == 'sort':
+        my_list.sort()
+    elif latest_command[0] == 'pop':
+        my_list.pop()
+    elif latest_command[0] == 'reverse':
+        my_list.reverse()
+    else:
+        print(my_list)
